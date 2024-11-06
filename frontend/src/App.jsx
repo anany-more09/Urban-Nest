671c7c7441c230030f70af5c56646e4acab27294
anany-more09 <ananymore63@gmail.com>
2024-11-06 11:30:43 +0530

    working

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='flex justify-center bg-black p-6 m-5 rounded-xl text-white text-2xl'>hello Urban Nest</div>
+    </>
+  )
+}
+
+export default App
